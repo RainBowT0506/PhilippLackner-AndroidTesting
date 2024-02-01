8b390f480475e2439e603847b56d77092612e0d2
RainBowT <rainbowt0506@gmail.com>
2024-02-01 14:57:51 +0800

    feat: Testing with Dagger-Hilt
    Implement Hilt for dependency injection in Android tests
    
    - Added TestAppModule to provide dependencies for test classes
    - Created HiltTestRunner to use HiltTestApplication for testing
    - Modified ShoppingDaoTest to use Hilt for dependency injection
    - Updated build.gradle to include HiltTestRunner
    - Ensured all tests pass with Hilt injection
    - enhances testing setup by integrating Hilt for dependency injection in Android tests, reducing boilerplate code and improving test organization

---
+++ app/src/androidTest/java/com/rainbowt0506/philipplackner_androidtesting/HillTestRunner.kt
@@ -0,0 +1,17 @@
+package com.rainbowt0506.philipplackner_androidtesting
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HillTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
